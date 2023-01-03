1c6387aab259c87f7407495721f26f446a23bf3d
James Connolly <jconnolly@tenstorrent.com>
2023-01-03 15:43:02 +0000

    modernizing cfg file

--- testing/configs.py
+++ testing/configs.py
@@ -1,59 +1,87 @@
 from ralph.test_entry import TestEntry
 from ralph.config_utils import check_valid
 from ralph.generators.simple import generate_simple_exec
+from ralph.validators import (create_cpi_validator, create_ipc_validator)
 
+import re
 
-TestEntry.set_default_whisper_path("build-Linux/whisper")
-#TestEntry.set_default_whisper_config_path("configuration/bh-tt-whisper.json")
+midcore_failing_tests = []
+def dependency_tests(override_dict={}):
+    default_config = dict(
+        ADD_latency=1,
+        ADD_throughput=6,
+        MUL_latency=3,
+        MUL_throughput=2,
+        DIV_latency=6,
+        DIV_throughput=1,
+        FADD_latency=1,
+        FADD_throughput=2,
+        FDIV_latency=6,
+        FDIV_throughput=1,
+        FMUL_latency=6,
+        FMUL_throughput=1
 
-def generate_dcache_tests():
-    rss_bench = "ubench/rss.c"
+    )
 
-    def rss_generator(rss: int):
-        compile_flags = f"-DRSS={rss}  -mcmodel=medany -static -std=gnu99 -O2 -nostdlib  -fno-tree-loop-distribute-patterns -DPREALLOCATE=1 -march=rv64imafdc -mabi=lp64d"
-        return TestEntry(name=f"rss_{rss}",
-                         path_to_benchmark=rss_bench,
-                         compile_flags=compile_flags,
-                         whisper_flags="--isa rv64imafdc --newlib",
-                         description=f"8-byte strided pointer chase across {rss} bytes; cache capacity test",
-                         whisper_cycles=300000000)
+    default_config.update(override_dict)
 
-    rss_tests = [rss_generator(rss) for rss in [32, 64, 128, 256, 512, 1024]]
-    return rss_tests
+    valid_inst = [
+        'ADD_V0', 'DIV_V0', 'MUL_V0', 'FADD.S_V0', 'FDIV.S_V0', 'FMUL.S_V0',
+        'FADD.D_V0', 'FDIV.D_V0', 'FMUL.D_V0']
 
-
-def get_microprobe_name(path: str) -> str:
-    return path.split('/')[-1].split('.')[0]
-
-
-def n_hop_tests(instruction="ADD_V0", desc="{dep_distance}-hop test"):
     tests = []
 
-    for dep_distance in range(1, 5):
-        test_name = f"{instruction}_{dep_distance}_hop"
-        # We generate a test with microprobe
-        test_path = generate_simple_exec(
-            name=f"{test_name}", instructions=instruction, dep_distance=dep_distance)
+    def n_hop_tests(instruction="ADD_V0", desc="{dep_distance}-hop test"):
+        tests = []
 
-        # A test entry is created
-        test = TestEntry.microprobe_test(name=test_name,
-                                         path_to_benchmark=test_path,
-                                         whisper_cycles=10000000)
-        tests.append(test)
+        for dep_distance in [1, 6]:
+            test_name = f"{instruction}_{dep_distance}_hop"
+            if dep_distance == 1:
+                # We generate a test with microprobe
+                key = f"{re.findall(r'[^ ._]*', instruction)[0]}_latency"
+                validator = create_cpi_validator(default_config[key], 0.1)
+            else:
+                # We generate a test with microprobe
+                key = f"{re.findall(r'[^ ._]*', instruction)[0]}_throughput"
+                validator = create_ipc_validator(default_config[key], 0.1)
+
+            test_path = generate_simple_exec(
+                name=f"{test_name}", instructions=instruction, dep_distance=dep_distance)
+
+            test = TestEntry.microprobe_test(name=f"{instruction}_{'throughput' if dep_distance == 6 else 'latency'}",
+                                             path_to_benchmark=test_path,
+                                             whisper_cycles=500000,
+                                             description=f"measures {key}",
+                                             validator=validator)
+
+            tests.append(test)
+        return tests
+
+    for inst in valid_inst:
+        tests += n_hop_tests(instruction=inst,
+                             desc=f"{{dep_distance}}-hop test with {inst} ")
     return tests
 
 
-def issue_tests():
-    return n_hop_tests()
-
-
-def memory_tests():
-    return n_hop_tests(instruction="LD_V0", desc="{dep_distance}-hop test with ld dw")
-
-
-all_tests = dict(dcache=generate_dcache_tests(), n_hop_tests=issue_tests(),
-                 memory_tests=memory_tests())
+all_tests = dict(
+    n_hop_tests=dependency_tests())
 
 
 if __name__ == "__main__":
     check_valid(all_tests, "trace")
+
+
+"""
+Test for max parallelism across different functional units
+
+
+
+Test window size (many dependants -> some independant inst)
+
+stream of loads/stores -> should be seeing saturating bw
+
+load to use latency
+
+
+
+"""
