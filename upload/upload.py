6afc8ad7a7f039b66ba3622afea65405ff900e98
James Connolly <jconnolly@tenstorrent.com>
2023-01-03 21:01:32 +0000

    Fix

--- upload/upload.py
+++ upload/upload.py
@@ -1,16 +1,16 @@
 import requests
 import os
-job_token = os.env["CI_JOB_TOKEN"]
-binary_name = os.env["CI_JOB_TOKEN"]
-UBUNTU_BINARY = os.env["UBUNTU_BINARY"]
-whisper_url = f"{os.env['PACKAGE_REGISTRY_URL']}/{UBUNTU_BINARY}"
-ralph_url = f"{os.env['RALPH_PACKAGE_REGISTRY_URL']}/{UBUNTU_BINARY}"
+job_token = os.environ["CI_JOB_TOKEN"]
+binary_name = os.environ["CI_JOB_TOKEN"]
+UBUNTU_BINARY = os.environ["UBUNTU_BINARY"]
+whisper_url = f"{os.environ['PACKAGE_REGISTRY_URL']}/{UBUNTU_BINARY}"
+ralph_url = f"{os.environ['RALPH_PACKAGE_REGISTRY_URL']}/{UBUNTU_BINARY}"
 
 
 def upload_file(url: str):
     file1 = dict(file=open(f"build-Linux/{UBUNTU_BINARY}", 'rb'))
 
-    requests.post(url=url, header={"JOB-TOKEN": os.env['CI_JOB_TOKEN']}, files=file1)
+    requests.post(url=url, header={"JOB-TOKEN": os.environ['CI_JOB_TOKEN']}, files=file1)
 
 
 upload_file(whisper_url)
